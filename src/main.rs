2dcb1d46d8f15648284bd2f0c30a1ce36e9c6a8b
Zijian Rao <raozijian@gmail.com>
2025-11-10 21:58:10 +0800

    feat: folder list supported

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
-use std::io;
+use std::{env, fs, io};
 
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
 use ratatui::{
-    buffer::Buffer,
-    layout::Rect,
+    layout::{Constraint, Layout, Rect},
     style::Stylize,
-    symbols::border,
     text::{Line, Text},
-    widgets::{Block, Paragraph, Widget},
+    widgets::{Block, List, ListItem, Paragraph},
     DefaultTerminal, Frame,
 };
 
@@ -33,7 +31,48 @@ impl App {
     }
 
     fn draw(&self, frame: &mut Frame) {
-        frame.render_widget(self, frame.area());
+        let vertical = Layout::vertical([Constraint::Min(1), Constraint::Length(1)]);
+        let [messages_area, help_area] = vertical.areas(frame.area());
+        self.draw_help_area(help_area, frame);
+        self.draw_messages_area(messages_area, frame);
+    }
+
+    fn draw_help_area(&self, area: Rect, frame: &mut Frame) {
+        let instructions = Text::from(Line::from(vec![
+            " List Folder ".into(),
+            "<Right>".blue().bold(),
+            " Quit ".into(),
+            "<Q> ".blue().bold(),
+        ]));
+        let help_message = Paragraph::new(instructions);
+        frame.render_widget(help_message, area);
+    }
+    fn draw_messages_area(&self, area: Rect, frame: &mut Frame) {
+        let instructions: Paragraph;
+        if self.list_enabled {
+            let current_dir = env::current_dir().unwrap();
+            let entries = fs::read_dir(&current_dir).unwrap();
+
+            let mut path_holder: Vec<ListItem> = Vec::new();
+            for entry in entries {
+                let entry = entry.unwrap();
+                let path = entry.path();
+                let list_item = ListItem::new(Line::from(path.to_string_lossy().into_owned()));
+                path_holder.push(list_item);
+            }
+            let messages = List::new(path_holder)
+                .block(Block::bordered().title(current_dir.to_string_lossy().into_owned()));
+            frame.render_widget(messages, area);
+        } else {
+            instructions = Paragraph::new(Text::from(Line::from(vec![
+                " List Folder ".into(),
+                "<Right>".blue().bold(),
+                " Quit ".into(),
+                "<Q> ".blue().bold(),
+            ])))
+            .block(Block::bordered().title("Viewer"));
+            frame.render_widget(instructions, area);
+        }
     }
     fn handle_events(&mut self) -> io::Result<()> {
         match event::read()? {
@@ -59,79 +98,3 @@ impl App {
         self.list_enabled = true;
     }
 }
-
-impl Widget for &App {
-    fn render(self, area: Rect, buf: &mut Buffer) {
-        let title = Line::from(" Athena Viewer (DEV) ".bold());
-        let instructions = Line::from(vec![
-            " List Folder ".into(),
-            "<Right>".blue().bold(),
-            " Quit ".into(),
-            "<Q> ".blue().bold(),
-        ]);
-        let block = Block::bordered()
-            .title(title.centered())
-            .title_bottom(instructions.centered())
-            .border_set(border::THICK);
-
-        let folder_info;
-        if self.list_enabled {
-            folder_info = Text::from(vec![Line::from(vec![
-                "LIST FOLDER TO BE IMPLEMENTED".into(),
-                "LIST FOLDER TO BE IMPLEMENTED".into(),
-            ])]);
-        } else {
-            folder_info = Text::from(vec![Line::from(vec!["EMPTY".into()])]);
-        }
-        Paragraph::new(folder_info)
-            .centered()
-            .block(block)
-            .render(area, buf);
-    }
-}
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//     use ratatui::style::Style;
-
-//     #[test]
-//     fn render() {
-//         let app = App::default();
-//         let mut buf = Buffer::empty(Rect::new(0, 0, 50, 4));
-
-//         app.render(buf.area, &mut buf);
-
-//         let mut expected = Buffer::with_lines(vec![
-//             "┏━━━━━━━━━━━━━ Counter App Tutorial ━━━━━━━━━━━━━┓",
-//             "┃                    Value: 0                    ┃",
-//             "┃                                                ┃",
-//             "┗━ Decrement <Left> Increment <Right> Quit <Q> ━━┛",
-//         ]);
-//         let title_style = Style::new().bold();
-//         let counter_style = Style::new().yellow();
-//         let key_style = Style::new().blue().bold();
-//         expected.set_style(Rect::new(14, 0, 22, 1), title_style);
-//         expected.set_style(Rect::new(28, 1, 1, 1), counter_style);
-//         expected.set_style(Rect::new(13, 3, 6, 1), key_style);
-//         expected.set_style(Rect::new(30, 3, 7, 1), key_style);
-//         expected.set_style(Rect::new(43, 3, 4, 1), key_style);
-
-//         assert_eq!(buf, expected);
-//     }
-
-//     #[test]
-//     fn handle_key_event() -> io::Result<()> {
-//         let mut app = App::default();
-//         app.handle_key_events(KeyCode::Right.into());
-//         assert_eq!(app.counter, 1);
-
-//         app.handle_key_events(KeyCode::Left.into());
-//         assert_eq!(app.counter, 0);
-
-//         let mut app = App::default();
-//         app.handle_key_events(KeyCode::Char('q').into());
-//         assert!(app.exit);
-
-//         Ok(())
-//     }
-// }
